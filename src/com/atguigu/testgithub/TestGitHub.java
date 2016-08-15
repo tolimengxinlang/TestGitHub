f0ac8b8c033affd00e9ebf814dc9c1051684c247
tolimengxinlang <tolimengxinlang@sina.com>
2016-08-15 19:46:26 +0800

    Update TestGitHub.java

--- src/com/atguigu/testgithub/TestGitHub.java
+++ src/com/atguigu/testgithub/TestGitHub.java
@@ -5,7 +5,6 @@ public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello github");
 		System.out.println("hello github 服务器01");
-		System.out.println("third row");
 		
 
 	}
