42e0590b4a30faa5d6bd379ab0efc02a14263637
tolimengxinlang <tolimengxinlang@sina.com>
2016-08-15 19:36:57 +0800

    update 01
    update 01

--- src/com/atguigu/testgithub/TestGitHub.java
+++ src/com/atguigu/testgithub/TestGitHub.java
@@ -6,9 +6,7 @@ public class TestGitHub {
 		System.out.println("hello github");
 		System.out.println("hello github 服务器01");
 		System.out.println("hello github 服务器02 by other");
-		System.out.println("hello github 服务器03 by other");
-		System.out.println("hello github 服务器04 by other");
-		System.out.println("hello github 服务器05 by other");
+	
 	}
 
 }
